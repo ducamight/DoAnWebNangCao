0844b78e8a76087492eb223b23d75149ec83bd8a
ducamight <triducnho3011@gmail.com>
2024-11-12 17:45:57 +0700

    Using jwt to authentication the user. Control the feature that normal user or admin can use.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -7,18 +8,31 @@ import { notFound, errorHandler } from './middleware/errormiddleware.js';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-connectDB(); // Connect to MongoDB
+
 
 const port = process.env.PORT || 5000;
 
+connectDB(); // Connect to MongoDB
+
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
