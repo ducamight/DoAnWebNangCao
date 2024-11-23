8c5c94c4691b9277f5f130e43c66f6efbe2b66d7
ducamight <triducnho3011@gmail.com>
2024-11-23 15:58:57 +0700

    Create searchBox

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,9 +7,9 @@ import Message from "../components/Message.jsx";
 import Paginate from "../components/Paginate.jsx";
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
   
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
   return (
     <>
@@ -24,7 +24,7 @@ const HomeScreen = () => {
                 </Col>
             ))}
         </Row>
-        <Paginate pages={data.pages} page={data.page} />
+        <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
       </>)}
     </>
   )
